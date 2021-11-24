a0b6e27ac4b4f9523876a74b35a9dc14d71a2ff5
VitorSolerAguilar <vitorsoleraguilar49@gmail.com>
2021-11-23 23:16:01 -0300

    Primeiro commit

---
+++ Tabuada/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Tabuada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Digite um numero para fazer a tabuada: ");
+                int Numero = int.Parse(Console.ReadLine());
+
+                for (int i = 1; i < 11; i++)
+                {
+                    int Resultado = Numero * i;
+                    Console.WriteLine(Numero + " X " + i + " = " + Resultado);
+                }
+            }catch(Exception Error)
+            {
+                Console.Write("Detalhe: " + Error);
+            }
+        }
+    }
+}
